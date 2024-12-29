066a5f3fdf174a9c50498c174762b62f85ce8708
Felipe Weber <git@felipeweber.dev.br>
2024-12-28 22:13:58 -0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SWTube - Uma Plataforma Star Wars</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body data-bs-theme="dark">
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">SWTube</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
