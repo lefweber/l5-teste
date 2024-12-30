9f07c476d62cba59832dda828c28dd2e2e04475d
Felipe Weber <git@felipeweber.dev.br>
2024-12-30 17:46:26 -0300

    change cards style

--- public/index.php
+++ public/index.php
@@ -106,113 +106,65 @@ require __DIR__ . '/../vendor/autoload.php';
 <main>
   <div class="container">
       <div class="row">
-          <div class="col-12 col-md-4 glow-div" style="cursor: pointer;">
+          <div class="col-12 col-md-4 glow-div">
             <div class="card">
               <img src="img/the-phantom-menace_small.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <a href="details.php" class="btn btn-outline-info">Mais Detalhes</a>
               </div>
             </div>
           </div>
-          <div class="col-12 col-md-4 glow-div" style="cursor: pointer;">
+          <div class="col-12 col-md-4 glow-div">
             <div class="card">
               <img src="img/return-of-the-jedi_small.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <a href="details.php" class="btn btn-outline-info">Mais Detalhes</a>
               </div>
             </div>
           </div>
-          <div class="col-12 col-md-4 glow-div" style="cursor: pointer;">
+          <div class="col-12 col-md-4 glow-div">
             <div class="card">
               <img src="img/a-new-hope_small.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <a href="details.php" class="btn btn-outline-info">Mais Detalhes</a>
               </div>
             </div>
           </div>
       </div>
       <div class="row row-space">
-          <div class="col-12 col-md-4 glow-div" style="cursor: pointer;">
+          <div class="col-12 col-md-4 glow-div">
             <div class="card">
               <img src="img/attack-of-the-clones_small.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <a href="details.php" class="btn btn-outline-info">Mais Detalhes</a>
               </div>
             </div>
           </div>
-          <div class="col-12 col-md-4 glow-div" style="cursor: pointer;">
+          <div class="col-12 col-md-4 glow-div">
             <div class="card">
               <img src="img/revenge-of-the-sith_small.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <a href="details.php" class="btn btn-outline-info">Mais Detalhes</a>
               </div>
             </div>
           </div>
-          <div class="col-12 col-md-4 glow-div" style="cursor: pointer;">
+          <div class="col-12 col-md-4 glow-div">
             <div class="card">
               <img src="img/the-empire-strikes-back_small.jpg" class="card-img-top" alt="...">
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-              </ul>
-              <div class="card-body">
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <a href="details.php" class="btn btn-outline-info">Mais Detalhes</a>
               </div>
             </div>
           </div>
