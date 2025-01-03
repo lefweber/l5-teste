790a0fd5523b7ae396fcfad1474372af05e0eef6
Felipe Weber <git@felipeweber.dev.br>
2025-01-02 22:11:48 -0300

    create error view and connect to external API

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,9 +4,50 @@ namespace App\Controllers;
 
 class HomeController extends Controller
 {
+
+  private $movies;
+
   public function index()
   {
-    $this->view('home', [], $this->css());
+    $this->getAllMoviesFromExternalApi();
+    $this->view('home', ['movies' => $this->movies], $this->css());
+  }
+
+  public function getAllMoviesFromExternalApi(): void
+  {
+    $url = 'https://swapi.tech/api/films';
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    curl_close($ch);
+
+    if ($httpCode === 200 && $response !== false) {
+      $data = json_decode($response, true);
+
+      if(isset($data['message']))
+        if($data['message'] == 'ok') {
+          $this->movies = $data;
+        }
+        else {
+          $this->view('error', [
+            'code' => 500,
+            'error' => 'API dos filmes Star Wars não está retornando dados. Verifique o Rate Limiting.',
+          ]);
+          exit;
+        }
+    } else {
+      $this->view('error', [
+        'code' => 500,
+        'error' => 'Não foi possível se comunicar com a API dos filmes Star Wars, por favor, verifique a sua conexão.',
+      ]);
+      exit;
+    }
   }
 
   private function css()
