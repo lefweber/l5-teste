ce3250ab38249c8b0ffda0330c7ed2c7edcb926f
Felipe Weber <git@felipeweber.dev.br>
2025-01-05 00:51:55 -0300

    bugfix / error message not showing

--- app/Views/Error.php
+++ app/Views/Error.php
@@ -1,5 +1,5 @@
 <main style="padding-top: 20vh; min-height: calc(100vh - 92px); padding-left: 1rem; padding-right: 1rem; text-align: center;">
   <svg style="width: 5rem; fill: #dc3545; margin-bottom: 2rem;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>
   <h1 style="margin-bottom: 1rem;">Ops, ocorreu algum erro! (<?php echo $code ?>)</h1>
-  <p><?php echo $error ?></p>
+  <p><?php echo $message ?></p>
 </main>
