6c7e89ebc4917ff10645fee646b437a8c56be059
Felipe Weber <git@felipeweber.dev.br>
2025-01-06 02:47:46 -0300

    add favicon

--- app/Views/Layout.php
+++ app/Views/Layout.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>SWTube - Uma Plataforma Star Wars</title>
+  <link rel="shortcut icon" href="/favicon.png">
   <link rel="stylesheet" href="/css/bootstrap.min.css">
   <style>
     #logo {
